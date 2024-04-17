9e7d9928641e293858f84434dd43a9b3c5ce3a27
steven <flyrobot27@gmail.com>
2024-04-17 12:23:37 -0600

    Remove initial XML

--- src/App.tsx
+++ src/App.tsx
@@ -7,9 +7,6 @@ import { workspaceHandler } from './workspaceHandlers';
 import './App.css'
 
 function App() {
-
-  const initialXml = '<xml xmlns="https://developers.google.com/blockly/xml"><block type="main_block" x="25" y="20"><field name="PROCESS_NAME">process name</field><field name="length_of_run">1</field><field name="no_of_cycles">1</field></block></xml>';
-
   Blockly.common.defineBlocks(blocks);
   Object.assign(cycloneGenerator.forBlock, forBlock);
   function setWorkspaceHandler(workspace: Blockly.WorkspaceSvg) {
@@ -20,7 +17,6 @@ function App() {
     <div>
       <BlocklyWorkspace
         toolboxConfiguration={toolbox}
-        initialXml={initialXml}
         className="fill-height"
         workspaceConfiguration={{
           grid: {
