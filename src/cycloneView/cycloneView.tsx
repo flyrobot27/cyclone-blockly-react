24015cffa4331691533ac985184cdcacfe0f8004
steven <flyrobot27@gmail.com>
2024-04-20 15:21:51 -0600

    Node Icon OK

--- src/cycloneView/cycloneView.tsx
+++ src/cycloneView/cycloneView.tsx
@@ -1,43 +1,48 @@
-import { Canvas } from "reaflow";
+import { Canvas, Icon, Node } from "reaflow";
+import combi from "./icons/combi.png";
 
 export function CycloneView() {
     return (
         <div className="h-[88vh]">
-            <Canvas 
-            readonly={true}
-            nodes={[{
-                id: '1',
-                text: '1',
-                icon: {
-                    url: "./icons/combi.png",
-                    height: 50,
-                    width: 50
-                }
-            }, {
-                id: '2',
-                text: '2'
-            }, {
-                id: '3',
-                text: '3'
-            },{
-                id: '4',
-                text: '4'
-            }]} edges={[{
-                id: '1-2',
-                from: '1',
-                to: '2'
-            }, {
-                id: '1-3',
-                from: '1',
-                to: '3'
-            },{
-                id: '1-4',
-                from: '1',
-                to: '4'
-            }, {
-                id: '4-1',
-                from: '4',
-                to: '1'
-            }]} onLayoutChange={layout => console.log('Layout', layout)} />
+            <Canvas className="bg-gray-100"
+                readonly={true}
+                nodes={[{
+                    id: '1',
+                    text: '1',
+                    icon: {
+                        url: combi,
+                        width: 50,
+                        height: 50
+                    }
+                }, {
+                    id: '2',
+                    text: '2'
+                }, {
+                    id: '3',
+                    text: '3'
+                }, {
+                    id: '4',
+                    text: '4'
+                }]}
+
+                edges={[{
+                    id: '1-2',
+                    from: '1',
+                    to: '2'
+                }, {
+                    id: '1-3',
+                    from: '1',
+                    to: '3'
+                }, {
+                    id: '1-4',
+                    from: '1',
+                    to: '4'
+                }, {
+                    id: '4-1',
+                    from: '4',
+                    to: '1'
+                }]}
+                onLayoutChange={layout => console.log('Layout', layout)}
+                node={<Node icon={<Icon/>}/>} />
         </div>);
 }
